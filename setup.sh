b957e5fa7ac85f0c3cf5c731120d597e5bcd71c3
victorbjelkholm <victorbjelkholm@gmail.com>
2019-05-21 17:20:34 +0200

    Initial commit
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env sh
+
+# awesome-rc.lua => ~/.config/awesome/rc.lua
+# qutebrowser-config.py => ~/.config/qutebrowser/config.py
+# tmux.conf => ~/.tmux.conf
+
+# More packages to install
+# Just a bunch of packages I use sometimes
+# youtube-dl
+# ffmpeg
+# zip/unzip
+# yubico
+# vagrant
+# virtualbox
+# valgrind
+# wine + vkd3d
+# visualvm
+# wacom
+# git
+# gimp
+# gnucash
+# golang
+# go-ipfs
+# godot
+# wireshark
+# keybase
+# graphviz
+# ipscan
+# telegram
+# tmux
+# irccloud
+# jq
+# krita
+# java/clojure/leingen
+# aria2c
+# asciinema
+# leocad
+# meld
+# nmap
+# nodejs/npm
+# obs-studio
+# wireguard
+# parallel
+# pv
+# python
+# qemu
+# rfc
+# recordmydesktop
+# ruby
+# scanmem
+# slack-desktop
+# spotify
+# steam
+# strace
+# tcpdump
+# aspell
+# audacity
+# autojump
+# zsh
+# blender
+# bluez
+# cdnjs
+# docker/containerd/docker-compose/docker-machine
+# crystal
+# curl
+# deluge or transmission-gtk
+# discord
+
+# Configs missing:
+# zshrc
+# git
+# alacritty
