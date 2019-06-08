883f7f1889ec62e9adc1fbcf72b16f18fb8f2687
victorbjelkholm <git@victor.earth>
2019-06-08 11:05:06 +0200

    Make setup install all configuration correctly
    for nixos only right now

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-#! /usr/bin/env sh
+#! /usr/bin/env sh 
+
+set -e
+
+current_dir=$(pwd)
+new_dir=/home/user
 
 # awesome-rc.lua => ~/.config/awesome/rc.lua
 # qutebrowser-config.py => ~/.config/qutebrowser/config.py
@@ -9,6 +14,17 @@
 # zshrc => ~/.zshrc
 # /keybase/private/diggan/docs => ~/docs
 # alacritty.yml => ~/.config/alacritty/alacritty.yml
+
+# Symlink configs into the right place
+ln -s $current_dir/awesome-rc.lua $new_dir/.config/awesome/rc.lua
+ln -s $current_dir/qutebrowser-config.py $new_dir/.config/qutebrowser/config.py
+ln -s $current_dir/tmux.conf $new_dir/.tmux.conf
+sudo ln -s $current_dir/configuration.nix /etc/nixos/configuration.nix
+ln -s $current_dir/gitconfig $new_dir/.gitconfig
+ln -s $current_dir/git-commit-template $new_dir/.git-commit-template
+ln -s $current_dir/zshrc $new_dir/.zshrc
+ln -s /keybase/private/diggan/docs $new_dir/docs
+ln -s $current_dir/alacritty.yml $new_dir/.config/alacritty/alacritty.yml
 
 # More packages to install
 # Just a bunch of packages I use sometimes
